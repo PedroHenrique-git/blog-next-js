5b562e239ff6995ccf6aa7a3eca11be6fb8ea13f
PedroHenrique-git <pedromf34@outlook.com>
2021-10-17 01:16:00 -0300

    configure styled components and provider test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import { ThemeProvider } from 'styled-components';
+import NameProvider from '../providers/nameProvider';
+import { GlobalStyles } from '../styles/global-styles';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <NameProvider>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </NameProvider>
+    </ThemeProvider>
+  );
 }
 export default MyApp;
